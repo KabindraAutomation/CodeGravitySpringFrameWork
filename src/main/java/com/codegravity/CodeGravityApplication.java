8afd50f23866d853f82eec0b99aea03c9faf3c9e
KabindraAutomation <kabindora@gmail.com>
2023-10-23 13:14:57 -0500

    Spring Framework CURD Operation

---
+++ src/main/java/com/codegravity/CodeGravityApplication.java
@@ -0,0 +1,13 @@
+package com.codegravity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGravityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGravityApplication.class, args);
+	}
+
+}
